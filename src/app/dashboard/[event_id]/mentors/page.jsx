a514f98e8324b939cf8f5e8a8af879b9677a48c1
imadfen <imadfen20@gmail.com>
2024-02-09 19:55:09 +0100

    finished the admin mentors page

--- src/app/dashboard/[event_id]/mentors/page.jsx
+++ src/app/dashboard/[event_id]/mentors/page.jsx
@@ -5,20 +5,20 @@ import { Icon } from '@iconify/react'
 
 export default function Page() {
   return (
-    <div className="flex h-screen text-white">
-      <div className='flex-grow'>
+    <div className="pt-5 flex h-screen text-white overflow-hidden">
+      <div className='flex-grow overflow-hidden'>
         <h2 className="text-3xl font-bold">Mentors</h2>
 
-        <div className='grid grid-cols-2'>
+        <div className='grid grid-cols-2 w-[85%]'>
           {
-            Array(4).fill(null).map((_, i) => (
-              <UserCard name={`mento ${i + 1}`} field="UI / UX" onButtonClick={() => { }} />
+            Array(6).fill(null).map((_, i) => (
+              <UserCard name={`mentor ${i + 1}`} field="UI / UX" onButtonClick={() => { }} />
             ))
           }
         </div>
 
       </div>
-      <div className='flex'>
+      <div className='flex m-5'>
         <button className='h-fit px-2 py-1 rounded-lg flex items-center gap-3 bg-[#0062D5]'>
           <Icon icon="mingcute:user-add-2-fill" width={24} />
           <p className="text-xl">New mentor</p>
