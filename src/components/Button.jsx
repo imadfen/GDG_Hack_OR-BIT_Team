e9ee2d07a03de90a665f5616fd4dae3a14be9af7
amelsadoun <amelsadoun.b@gmail.com>
2024-02-09 13:11:46 +0100

    added homepage, stylized card and button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Button({children}) {
   return (
-    <div className='flex items-center justify-center bg-gradient-to-br from-slate-400 via-transparent to-slate-400 rounded-[30px] h-10 p-[1px] w-48'>
+    <div className='flex items-center justify-center bg-gradient-to-br from-slate-400 via-transparent to-slate-400 rounded-[30px] h-12 p-[1px] w-48'>
     <button className=' flex-1 self-center justify-self-center ml-[0.5px] w-full h-full rounded-[30px] bg-gradient-to-br  from-slate-800 to-slate-600 z-10 text-white text-[16px]'>{children}</button>   
          </div>
   )
