bc5b37620f5b39bbd7c76f19b6b4f263c9ef467d
assiakheddia <assia.kheddia@icloud.com>
2024-02-09 15:35:08 +0000

    test

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,23 +1,12 @@
-<<<<<<< HEAD
-import Link from "next/link";
-import Login from "../pages/login"
-import { NoEvent } from "../pages/NoEvent";
-import { Feedback } from "../pages/Feedback";
-import { ParticipantHomePage } from "../pages/ParticipantHomePage";
-=======
 "use client"
 
 import NewEventForm from "@/components/NewEventForm";
 import { NoEvent } from "@/pages/NoEvent";
 import { useState } from "react";
 
->>>>>>> 8dd024b26f3e2873999c5f26d23425aaefdf3f06
 export default function Home() {
   const [isNewEvent, setIsNewEvent] = useState(false);
   return (
-<<<<<<< HEAD
-    <ParticipantHomePage></ParticipantHomePage>
-=======
     <>
       {isNewEvent ?
         <NewEventForm onCancel={() => setIsNewEvent(false)} onCreate={() => { }} />
@@ -25,6 +14,5 @@ export default function Home() {
         <NoEvent onNewEvent={() => setIsNewEvent(true)} />
       }
     </>
->>>>>>> 8dd024b26f3e2873999c5f26d23425aaefdf3f06
   );
 }
