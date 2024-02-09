728c568ab746704a3dd14effc9950f091f00504a
imadfen <imadfen20@gmail.com>
2024-02-09 12:12:25 +0100

    finished the createNewEvent form

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,10 @@
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
+import { getServerSession } from "next-auth";
+import { options } from "./api/auth/[...nextauth]";
+// import { redirect } from "next/navigation";
+// import { SessionProvider } from "next-auth/react";
+// import { headers } from "next/headers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,10 +13,30 @@ export const metadata = {
   description: "GDG-Hack Or-Bit project",
 };
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
+  // const session = await getServerSession(options);
+
+  // const headersList = headers();
+  // const pathname = headersList.get('next-url') || '';
+  // console.log('Current pathname:', pathname);
+
+  // if (!session) {
+  //   redirect("/login");
+  //   return null
+  // }
+
+
   return (
+    // <SessionProvider session={session}>
     <html lang="en">
-      <body className={`cursor-default ${inter.className}`}>{children}</body>
+      <body className={`cursor-default ${inter.className}`}>
+        {/* {session ? */}
+        {children}
+        {/* :
+          null
+        } */}
+      </body>
     </html>
+    // </SessionProvider>
   );
 }
--- src/pages/NoEvent.jsx
+++ src/pages/NoEvent.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import VideoBackground from '../components/VideoBackground';
-export const NoEvent = () => {
+
+
+export const NoEvent = ({ onNewEvent }) => {
   return (
     <div>
       <VideoBackground />
@@ -10,7 +12,7 @@ export const NoEvent = () => {
           alt=""
           className="h-10 w-auto absolute top-0 left-0 ml-4 mt-4"
         />
-      <img
+        <img
           src="/GdgLogo.svg"
           alt=""
           className="h-5 w-auto absolute bottom-0 left-0 ml-5 mb-7"
@@ -19,10 +21,10 @@ export const NoEvent = () => {
           No current events
         </h1>
         <div className='flex justify-center mt-20'>
-        <button className='text-white mr-8 border border-white px-5 py-5 rounded-lg min-w-[150px] w-5 '>Archive of events</button>
-        <button className='text-white ml-20 border border-white px-4 py-2 rounded-lg min-w-[150px]'>Create event</button>
+          <button className='text-white mr-8 border border-white px-5 py-5 rounded-lg min-w-[150px] w-5 '>Archive of events</button>
+          <button className='text-white ml-20 border border-white px-4 py-2 rounded-lg min-w-[150px]' onClick={onNewEvent} >Create event</button>
         </div>
-        
+
       </div>
     </div>
   );
