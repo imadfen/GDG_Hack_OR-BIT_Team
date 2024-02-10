39092a189ec642c9d8c670e7d0ee0db10c4c6d23
imadfen <imadfen20@gmail.com>
2024-02-10 03:03:47 +0100

    started doing all the pop-ups

--- src/app/dashboard/[event_id]/mentors/page.jsx
+++ src/app/dashboard/[event_id]/mentors/page.jsx
@@ -1,29 +1,39 @@
 "use client"
 
+import CreateMentorForm from '@/components/CreateMentorForm';
 import UserCard from '@/components/UserCard'
 import { Icon } from '@iconify/react'
+import { useState } from 'react';
 
 export default function Page() {
+  const [isAddMentor, setIsAddMentor] = useState(false);
+
   return (
     <div className="pt-5 flex h-screen text-white overflow-hidden">
-      <div className='flex-grow overflow-hidden'>
-        <h2 className="text-3xl font-bold">Mentors</h2>
+      {isAddMentor ?
+        <CreateMentorForm onCancel={() => setIsAddMentor(false)} onCreate={() => setIsAddMentor(false)} />
+        :
+        <>
+          <div className='flex-grow overflow-hidden'>
+            <h2 className="text-3xl font-bold">Mentors</h2>
 
-        <div className='grid grid-cols-2 w-[85%]'>
-          {
-            Array(6).fill(null).map((_, i) => (
-              <UserCard name={`mentor ${i + 1}`} field="UI / UX" onButtonClick={() => { }} />
-            ))
-          }
-        </div>
+            <div className='grid grid-cols-2 w-[85%]'>
+              {
+                Array(6).fill(null).map((_, i) => (
+                  <UserCard name={`mentor ${i + 1}`} field="UI / UX" onButtonClick={() => { }} />
+                ))
+              }
+            </div>
 
-      </div>
-      <div className='flex m-5'>
-        <button className='h-fit px-2 py-1 rounded-lg flex items-center gap-3 bg-[#0062D5]'>
-          <Icon icon="mingcute:user-add-2-fill" width={24} />
-          <p className="text-xl">New mentor</p>
-        </button>
-      </div>
+          </div>
+          <div className='flex m-5'>
+            <button className='h-fit px-2 py-1 rounded-lg flex items-center gap-3 bg-[#0062D5]' onClick={() => setIsAddMentor(true)}>
+              <Icon icon="mingcute:user-add-2-fill" width={24} />
+              <p className="text-xl">New mentor</p>
+            </button>
+          </div>
+        </>
+      }
     </div>
   )
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function Button({children}) {
+function Button({children, onClick}) {
   return (
-    <div className='flex items-center justify-center bg-gradient-to-br from-slate-400 via-transparent to-slate-400 rounded-[30px] h-12 p-[1px] w-48'>
+    <div className='flex items-center justify-center bg-gradient-to-br from-slate-400 via-transparent to-slate-400 rounded-[30px] h-12 p-[1px] w-48' onClick={onClick}>
     <button className=' flex-1 self-center justify-self-center ml-[0.5px] w-full h-full rounded-[30px] bg-gradient-to-br  from-slate-800 to-slate-600 z-10 text-white text-[16px]'>{children}</button>   
          </div>
   )
